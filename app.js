1740cd5169b03e7d3cea7ad2705ebc05610cc43d
tobudim <tobudim@protonmail.com>
2020-09-14 19:40:31 +0200

    initiate app structure

---
+++ app.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+const config = require(path.join(__dirname, "config.json"));
+
+const urls = {
+  monster: "https://monster.fr",
+  indeed: "https://indeed.fr",
+  poleEmploi: "https://pole-emploi.fr",
+};
